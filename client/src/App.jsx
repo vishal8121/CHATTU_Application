40061e008c66d89fc396dd5c0094140283da292a
Vishal Kumar <vishalkumar34205@gmail.com>
2024-06-14 21:12:31 +0530

    import lazy and functions from react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-// rafce shortcuts for the react arrow function
+/* rafce shortcuts for the react arrow function
+*/
+import {React, lazy} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
-import React from 'react'
+// lazy function for dynamic import pages
+const Home = lazy(()=> import('../pages/Home'));
+const Login = lazy(()=> import('../pages/Login'));
+const Chat = lazy(()=> import('../pages/Chat'));
+const Groups = lazy(()=> import('../pages/Groups'));
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={< Home />} />
+    <Route path='/chat/:chatId' element={< Chat />} />
+    <Route path='/' element={< Groups />} />
+    <Route path='/login' element={< Login />} />
+  </Routes>
+  
+  </BrowserRouter>
 }
 
 export default App
