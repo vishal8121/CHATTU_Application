9be8c5bb498e09fc414756cdf0fe7c75906195e1
Vishal Kumar <vishalkumar34205@gmail.com>
2024-06-15 18:36:43 +0530

    create login page

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Container, Paper, TextField, Typography } from '@mui/material';
 
 const Login = () => {
+
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleLogin = () => setIsLogin(false);
+
+
   return (
-    <div>Login</div>
-  )
-}
+    <Container component={"main"} maxWidth="sx">
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+
+        {
+          isLogin ? (
+            <>
+              <Typography variant="h5"> Login</Typography>
+              <form>
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin="normal"
+                  variant="outlined"
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                />
+
+                <Button sx={{
+                  marginTop: "1rem",
+                }} variant="contained" color="primary" type="submit"
+                >
+                  Login
+                </Button>
+
+                <Typography>Or</Typography>
+
+                <Button sx={{
+                  marginTop: "1rem",
+                }}
+                  variant="contained"
+                  color="secondary"
+                  onclick={toggleLogin}
+                >
+                  Sign
+                </Button>
+              </form>
+            </>
+
+          ) : <span>register</span>
+        }
+
+      </Paper>
+
+    </Container >
+  );
+};
 
 export default Login
